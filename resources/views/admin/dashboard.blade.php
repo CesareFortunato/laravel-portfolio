7ef5bdb45f39b37195069797bdd548f238ea2721
CesareFortunato <cesare.fortunato89@gmail.com>
2026-04-23 18:06:47 +0200

    feat: button projects

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,4 +4,8 @@
     <h2 class="text-2xl font-bold mb-4">Dashboard Admin</h2>
 
     <p>Benvenuto nell'area amministratore!</p>
+
+    <a href="{{ route('admin.projects.index') }}" class="bg-blue-600 text-blue px-4 py-2 rounded">
+        Vai ai Progetti
+    </a>
 @endsection
