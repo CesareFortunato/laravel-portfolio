1b0e6288941ff827ec711c80fad89bc6f2a14853
CesareFortunato <cesare.fortunato89@gmail.com>
2026-04-28 10:45:46 +0200

    feat: delete

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.admin')
 
 @section('content')
+
+    <div class="d-flex py-4 gap-2">
+        <a class="btn btn-outline-warning" href="{{ route("admin.projects.edit", $project) }}">Modifica</a>
+        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            Elimina
+        </button>
+
+
+    </div>
+
     <div class="container mx-auto p-6">
 
         <a href="{{ route('admin.projects.index') }}" class="text-blue-600 underline mb-4 inline-block">
@@ -42,3 +52,28 @@
         </div>
     </div>
 @endsection
+
+
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-center">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Elimina il progetto</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Vuoi eliminare il progetto?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                <form action="{{ route("admin.projects.destroy", $project) }}" method="POST">
+                    @csrf
+                    @method("DELETE")
+                    <input type="submit" class="btn btn-outline-danger" value="Elimina">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
